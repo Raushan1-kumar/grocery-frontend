f7f77ff8d2a2249cc167170caea750fd48fbade6
raushan <raushansahu147@gmail.com>
2025-08-16 18:21:10 +0530

    final touch

--- src/pages/ProductForm.jsx
+++ src/pages/ProductForm.jsx
@@ -96,7 +96,7 @@ const ProductForm = () => {
     }
 
     try {
-      const response = await fetch('https://grocery-backend-s1kk.onrender.com/api/products', {
+      const response = await fetch('http://localhost:5000/api/products', {
         method: 'POST',
         body: formData
       });
